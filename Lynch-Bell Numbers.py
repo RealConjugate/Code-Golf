b2eb2e0ccefb48751fc2bf91681cda294fdb6ffd
A.T <61085515+RealConjugate@users.noreply.github.com>
2021-04-10 22:04:06 +0100

    Update Lynch-Bell Numbers.py

--- Lynch-Bell Numbers.py
+++ Lynch-Bell Numbers.py
@@ -14,6 +14,3 @@ print(f("126"))
 print(f("54"))
 print(f("55"))
 print(f("3915"))
-    
-# without whitespace removed
-# def f(n): s="" for i in n: if i in s or int(n)%int(i)!=0: return False s=s+i return True
