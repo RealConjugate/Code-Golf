f0e7d6124c36a9d95b44509935d91f28ddc16a35
A.T <61085515+RealConjugate@users.noreply.github.com>
2021-04-10 22:03:54 +0100

    Update Range, Reverse, Sum.py

--- Range, Reverse, Sum.py
+++ Range, Reverse, Sum.py
@@ -17,6 +17,3 @@ print(f(21))
 print(f(58))
 print(f(75))
 print(f(999))
-
-# whitespace removed
-# def f(n): s=0 for i in range(1,n+1): r="" k=str(i) for j in range(0,len(k)): r=r+k[len(k)-1-j] s+=int(r) return s
